cac6a1977fb8f865be909a25773334ff689df8d7
moonk <mkjoehsb@gmail.com>
2023-05-16 14:52:17 +0900

    ver2. bar

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from dash import Dash, html, dcc
 import plotly.express as px
 import pandas as pd
-from data import country_df
+from data import country_df, global_df
 from builders import make_table
+
 
 stylesheets = [
     "https://cdn.jsdelivr.net/npm/reset-css@5.0.1/reset.min.css",
@@ -11,6 +12,41 @@ stylesheets = [
 app = Dash(
     __name__, external_stylesheets=stylesheets
 )
+
+map = px.scatter_geo(
+    country_df,
+    size="Confirmed",
+    size_max=50,
+    template="plotly_dark",
+    locations="Country_Region",
+    locationmode="country names",
+    color="Confirmed",
+    hover_name="Country_Region",
+    hover_data={
+        "Country_Region": False,
+        "Confirmed": ":,2f",
+        "Deaths": True,
+        "Recovered": True,
+    },
+)
+
+bar = px.bar(
+    global_df,
+    x="condition",
+    y="count",
+    color=["Confirmed", "Deaths", "Recovered"],
+    template="plotly_dark",
+    title="Global Cases",
+    labels={
+        "condition": "Condition",
+        "count": "Count",
+        "color": "Condition",
+    },
+)
+# bar.update_layout(
+#     xaxis={"title": "Condition"},
+#     yaxis=dict(title="Count"),
+# )
 
 app.layout = html.Div(
     style={
@@ -36,32 +72,23 @@ app.layout = html.Div(
             children=[
                 html.Div(
                     children=[
+                        dcc.Graph(figure=map)
+                    ]
+                ),
+                html.Div(
+                    children=[
+                        dcc.Graph(figure=bar)
+                    ]
+                ),
+                html.Div(
+                    children=[
                         make_table(country_df)
                     ]
-                )
+                ),
             ]
         ),
     ],
 )
-
-
-fig = px.scatter_geo(
-    country_df,
-    size="Confirmed",
-    size_max=50,
-    template="plotly_dark",
-    locations="Country_Region",
-    locationmode="country names",
-    color="Confirmed",
-    hover_name="Country_Region",
-    hover_data={
-        "Country_Region": False,
-        "Confirmed": ":,2f",
-        "Deaths": True,
-        "Recovered": True,
-    },
-)
-fig.show()
 
 
 if __name__ == "__main__":
